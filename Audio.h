4ba04c78e5748f47cefc172bc682c5d02abb09a2
Paul Bender <paul.bender@acm.org>
2022-09-13 16:40:32 -0400

    refactor

---
+++ Audio.h
@@ -0,0 +1,17 @@
+/* Speak Jet routines */
+void speakJetReset();
+void speakJetWait();
+//Send "Hotbox detector, milepost" to the SpeakJet module
+void readWelcome();
+// read the mile post number, one digit at a time.
+void readMilepost(char []);
+void readNoDefects();
+void readDetectorOut();
+// read a single digit
+void speakdigit(char digit);
+//Calculate the speed
+float calcSpeed(int first,int last);
+//Read the speed in MPH.
+void readspeed(float speed);
+// Defect Alarm
+void defect_alarm();
